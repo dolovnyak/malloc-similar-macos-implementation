b6370fc474cddb35caca7ebedb2fa905c99f97f3
dolovnyak <dolovnyak@yandex.ru>
2022-06-26 08:09:25 +0300

    added node header getters/setters with tests

--- standard_malloc_implementation/tests/utilities_test.cpp
+++ standard_malloc_implementation/tests/utilities_test.cpp
@@ -1,7 +1,89 @@
 #include <gtest/gtest.h>
 
-#include "utilities.h"
+#include <tuple>
 
 extern "C" {
 #include "malloc_internal.h"
+#include "utilities.h"
 }
+
+void print_bits(uint64_t value) {
+    for (int i = 63; i >= 0; --i) {
+        if ((i + 1) % 8 == 0) {
+            std::cout << " ";
+        }
+        std::cout << ((value & (1ULL << i)) ? 1 : 0);
+    }
+}
+
+template<class T>
+using t_get_func_ptr = T(*)(const BYTE*);
+
+template<class T>
+using t_set_func_ptr = void (*)(BYTE*, T);
+
+template<class T>
+struct TestStruct {
+    std::vector<T> test_values;
+    std::vector<t_get_func_ptr<T>> get_value_func_list;
+    std::vector<t_set_func_ptr<T>> set_value_func_list;
+    std::vector<std::string> func_name_list;
+};
+
+template<class T>
+struct Node_Header_Operations_Test : public ::testing::Test {
+    static TestStruct<T> test_struct;
+};
+
+TYPED_TEST_SUITE_P(Node_Header_Operations_Test);
+
+TYPED_TEST_P(Node_Header_Operations_Test, Check_All) {
+    using TestClass = Node_Header_Operations_Test<TypeParam>;
+
+    BYTE node_header[16];
+
+    for (size_t i = 0; i < TestClass::test_struct.get_value_func_list.size(); ++i) {
+        bzero(node_header, sizeof(BYTE) * 16);
+
+        auto ptr_get_func = TestClass::test_struct.get_value_func_list[i];
+        auto ptr_set_func = TestClass::test_struct.set_value_func_list[i];
+
+        ASSERT_EQ(ptr_get_func(node_header), (TypeParam)0) << "Func type: " << TestClass::test_struct.func_name_list[i];
+
+        for (auto value: TestClass::test_struct.test_values) {
+            ptr_set_func(node_header, value);
+            std::cout << "left bits part:  ";
+            print_bits(*(int64_t*)node_header);
+            std::cout << std::endl;
+            std::cout << "right bits part: ";
+            print_bits(*((int64_t*)node_header + 1));
+            std::cout << std::endl;
+            std::cout << std::endl;
+
+            ASSERT_EQ(ptr_get_func(node_header), value) << "Func type: " << TestClass::test_struct.func_name_list[i];
+        }
+    }
+}
+
+REGISTER_TYPED_TEST_SUITE_P(Node_Header_Operations_Test, Check_All);
+
+typedef ::testing::Types<uint64_t, BOOL, t_allocation_type> Types;
+INSTANTIATE_TYPED_TEST_SUITE_P(Node_Header_Operations, Node_Header_Operations_Test, Types);
+
+template<> TestStruct<uint64_t> Node_Header_Operations_Test<uint64_t>::test_struct{
+        {1,             16, 128,                   16777215},
+        {get_node_size, get_large_node_size, get_previous_node_size, get_node_zone_start_offset, get_next_free_node_zone_start_offset},
+        {set_node_size, set_large_node_size, set_previous_node_size, set_node_zone_start_offset, set_next_free_node_zone_start_offset},
+        {"node_size", "large_node_size", "previous_node_size", "node_zone_start_offset", "next_free_node_zone_start_offset"}
+};
+template<> TestStruct<BOOL> Node_Header_Operations_Test<BOOL>::test_struct{
+        {TRUE, FALSE, TRUE, TRUE},
+        {get_node_available},
+        {set_node_available},
+        {"node_available"}};
+template<> TestStruct<t_allocation_type> Node_Header_Operations_Test<t_allocation_type>::test_struct{
+        {Tiny, Small, Large, Tiny},
+        {get_node_allocation_type},
+        {set_node_allocation_type},
+        {"node_allocation_type"}
+};
--- standard_malloc_implementation/utilities.h
+++ standard_malloc_implementation/utilities.h
@@ -3,67 +3,84 @@
 #include "malloc_internal.h"
 
 #define MASK_NODE_SIZE 0xFFFFFF0000000000
-#define MASK_NODE_SIZE_SHIFT 40
+#define SHIFT_NODE_SIZE 40
 
 #define MASK_PREV_NODE_SIZE 0x000000FFFFFF0000
-#define MASK_PREV_NODE_SIZE_SHIFT 16
+#define SHIFT_PREV_NODE_SIZE 16
 
 #define MASK_NODE_ZONE_START_OFFSET 0xFFFFFF0000000000
-#define MASK_NODE_ZONE_START_OFFSET_SHIFT 40
+#define SHIFT_NODE_ZONE_START_OFFSET 40
 
-#define MASK_NODE_NEXT_FREE_NODE_OFFSET 0x000000FFFFFF0000
-#define MASK_NODE_NEXT_FREE_NODE_OFFSET_SHIFT 16
+#define MASK_NEXT_FREE_NODE_OFFSET 0x000000FFFFFF0000
+#define SHIFT_NEXT_FREE_NODE_OFFSET 16
 
 #define MASK_NODE_AVAILABLE 0x0000000000000004
-#define MASK_NODE_AVAILABLE_SHIFT 2
+#define SHIFT_NODE_AVAILABLE 2
 
 #define MASK_NODE_TYPE 0x0000000000000003
-#define MASK_NODE_TYPE_SHIFT 0
+#define SHIFT_NODE_TYPE 0
+
+static inline uint64_t get(uint64_t variable_to_get, uint64_t mask, uint8_t shift) {
+    return (variable_to_get & mask) >> shift;
+}
+
+static inline void set(uint64_t* variable_to_set, uint64_t mask, uint8_t shift, uint64_t value) {
+    *variable_to_set &= ~mask; // set bits in value region to 0
+    *variable_to_set |= (value << shift); // set value in value region
+}
 
 static inline uint64_t get_node_size(const BYTE* node_header) {
+    return get(*(uint64_t*)node_header, MASK_NODE_SIZE, SHIFT_NODE_SIZE);
 }
 
 static inline void set_node_size(BYTE* node_header, uint64_t size) {
+    set((uint64_t*)node_header, MASK_NODE_SIZE, SHIFT_NODE_SIZE, size);
 }
 
 static inline uint64_t get_large_node_size(const BYTE* node_header) {
+    return *(uint64_t*)node_header;
 }
 
 static inline void set_large_node_size(BYTE* node_header, uint64_t size) {
+    *(uint64_t*)node_header = size;
 }
 
 static inline uint64_t get_previous_node_size(const BYTE* node_header) {
+    return get(*(uint64_t*)node_header, MASK_PREV_NODE_SIZE, SHIFT_PREV_NODE_SIZE);
 }
 
 static inline void set_previous_node_size(BYTE* node_header, uint64_t size) {
+    set((uint64_t*)node_header, MASK_PREV_NODE_SIZE, SHIFT_PREV_NODE_SIZE, size);
 }
 
 static inline uint64_t get_node_zone_start_offset(const BYTE* node_header) {
+    return get(*((uint64_t*)node_header + 1), MASK_NODE_ZONE_START_OFFSET, SHIFT_NODE_ZONE_START_OFFSET);
 }
 
 static inline void set_node_zone_start_offset(BYTE* node_header, uint64_t size) {
+    set((uint64_t*)node_header + 1, MASK_NODE_ZONE_START_OFFSET, SHIFT_NODE_ZONE_START_OFFSET, size);
 }
 
 static inline uint64_t get_next_free_node_zone_start_offset(const BYTE* node_header) {
+    return get(*((uint64_t*)node_header + 1), MASK_NEXT_FREE_NODE_OFFSET, SHIFT_NEXT_FREE_NODE_OFFSET);
 }
 
 static inline void set_next_free_node_zone_start_offset(BYTE* node_header, uint64_t size) {
+    set((uint64_t*)node_header + 1, MASK_NEXT_FREE_NODE_OFFSET, SHIFT_NEXT_FREE_NODE_OFFSET, size);
 }
 
 static inline BOOL get_node_available(const BYTE* node_header) {
-    return *((uint64_t*)node_header + 1) & MASK_NODE_AVAILABLE;
+    return (BOOL)get(*((uint64_t*)node_header + 1), MASK_NODE_AVAILABLE, SHIFT_NODE_AVAILABLE);
 }
 
 static inline void set_node_available(BYTE* node_header, BOOL available) {
-    uint64_t tmp = *((uint64_t*)node_header + 1);
-    tmp = tmp & ~MASK_NODE_AVAILABLE;
-    tmp = tmp | available << MASK_NODE_AVAILABLE_SHIFT;
+    set((uint64_t*)node_header + 1, MASK_NODE_AVAILABLE, SHIFT_NODE_AVAILABLE, (uint64_t)available);
 }
 
-static inline t_allocation_type get_node_allocation_type(BYTE* node_header) {
-
+static inline t_allocation_type get_node_allocation_type(const BYTE* node_header) {
+    return (t_allocation_type)get(*((uint64_t*)node_header + 1), MASK_NODE_TYPE, SHIFT_NODE_TYPE);
 }
 
-static inline void set_node_allocation_type(const BYTE* node_header, t_allocation_type type) {
-
+static inline void set_node_allocation_type(BYTE* node_header, t_allocation_type type) {
+    set((uint64_t*)node_header + 1, MASK_NODE_TYPE, SHIFT_NODE_TYPE, (uint64_t)type);
 }
