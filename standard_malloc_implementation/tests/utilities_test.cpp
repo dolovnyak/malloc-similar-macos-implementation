70e385a7de8a7167e1cec8fc5abb8104a0ab7a5e
dolovnyak <dolovnyak@yandex.ru>
2022-06-26 08:27:11 +0300

    up tests

--- standard_malloc_implementation/tests/utilities_test.cpp
+++ standard_malloc_implementation/tests/utilities_test.cpp
@@ -42,6 +42,9 @@ TYPED_TEST_P(Node_Header_Operations_Test, Check_All) {
         ASSERT_EQ(ptr_get_func(node_header), (TypeParam)0) << "Func type: " << TestClass::test_struct.func_name_list[i];
 
         for (auto value: TestClass::test_struct.test_values) {
+            if ((uint64_t)value > 16777215 && (void*)ptr_get_func != (void*)get_large_node_size) {
+                continue;
+            }
             ptr_set_func(node_header, value);
             ASSERT_EQ(ptr_get_func(node_header), value) << "Func type: " << TestClass::test_struct.func_name_list[i];
         }
@@ -54,7 +57,7 @@ typedef ::testing::Types<uint64_t, BOOL, t_allocation_type> Types;
 INSTANTIATE_TYPED_TEST_SUITE_P(Node_Header_Operations, Node_Header_Operations_Test, Types);
 
 template<> TestStruct<uint64_t> Node_Header_Operations_Test<uint64_t>::test_struct{
-        {1,             16,                  128,                    16777215},
+        {1,             16,                  128,                    16777215, 134523452345, SIZE_MAX - 1},
         {get_node_size, get_large_node_size, get_previous_node_size, get_node_zone_start_offset, get_next_free_node_zone_start_offset},
         {set_node_size, set_large_node_size, set_previous_node_size, set_node_zone_start_offset, set_next_free_node_zone_start_offset},
         {"node_size",   "large_node_size",   "previous_node_size",   "node_zone_start_offset",   "next_free_node_zone_start_offset"}
