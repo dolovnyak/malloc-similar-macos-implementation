18ff3ff4750823850bcab31242a73faa1ca5bef0
dolovnyak <dolovnyak@yandex.ru>
2022-07-15 05:26:35 +0300

    up cmake up build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cmake -S . -B build -D SAFE_FREE=true
 else
 cmake -S . -B build
 fi
-make -C build
+make -C build -j 8
 
 # install_name_tool -add_rpath ./ ./a.out <- through static link
 # DYLD_LIBRARY_PATH=$PWD DYLD_INSERT_LIBRARIES=libft_malloc_x86_64_Darwin.so DYLD_FORCE_FLAT_NAMESPACE=1 ./a.out <- through dynamic link
